5476799460d756f71a069ec08d73afc9af8bfed0
Abhinavcsv <abhiatrock7@gmail.com>
2026-07-20 10:49:49 +0530

    Added Reports and Settings pages

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,44 +4,28 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "74.125.140.108", // smtp.gmail.com IPv4
+  host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
-  tls: {
-    rejectUnauthorized: false,
-  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-// Verify SMTP connection on server startup
-transporter.verify((error, success) => {
-  if (error) {
-    console.error("❌ SMTP Error:", error);
-  } else {
-    console.log("✅ SMTP Server is Ready");
-  }
-});
-
 const sendEmail = async (to, subject, html) => {
   try {
-    console.log("📧 Sending email to:", to);
-
     const info = await transporter.sendMail({
-      from: `"OrbitCRM Team" <${process.env.EMAIL_USER}>`,
+      from: `"OrbitCRM" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html,
     });
 
-    console.log("✅ Email Sent:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("❌ Email Send Error:", error);
-    throw error;
+    console.log("✅ Mail Sent:", info.messageId);
+  } catch (err) {
+    console.error("❌ Mail Error:", err);
+    throw err;
   }
 };
 
