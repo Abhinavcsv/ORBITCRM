e895f6a9c441899b4d5fdd0296422a0e6787b326
Abhinavcsv <abhiatrock7@gmail.com>
2026-07-20 11:06:12 +0530

    Added Reports and Settings pages

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,13 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config();
-
 import nodemailer from "nodemailer";
+
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -16,7 +15,6 @@ const transporter = nodemailer.createTransport({
 
 const sendEmail = async (to, subject, html) => {
   try {
-    // Verify SMTP connection
     await transporter.verify();
     console.log("✅ SMTP Connected");
 
