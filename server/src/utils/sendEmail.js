36fbb8133da3bf4e985d4a3565867065f95267b3
Abhinavcsv <abhiatrock7@gmail.com>
2026-07-20 10:40:25 +0530

    Added Reports and Settings pages

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -4,9 +4,13 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "74.125.140.108", // smtp.gmail.com IPv4
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  tls: {
+    rejectUnauthorized: false,
+  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
